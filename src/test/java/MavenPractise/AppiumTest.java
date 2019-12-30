10498f9c8fac4e87f0e270ff5f32723d3c10d6ce
Swarantej <swaran.tej@gmail.com>
2019-12-30 22:28:22 +0530

    devleoptest branch commit

--- src/test/java/MavenPractise/AppiumTest.java
+++ src/test/java/MavenPractise/AppiumTest.java
@@ -10,4 +10,18 @@ public class AppiumTest {
 		System.out.println("Maven Plugin test from Appium test file");
 	}
 
+@Test
+	
+	public void appiumTest1() {
+		System.out.println("Dvelop branch");
+	}
+
+
+@Test
+
+public void appiumTest2() {
+	System.out.println("Maven Plugin test from Appium test file 12&");
+}
+
+	
 }
